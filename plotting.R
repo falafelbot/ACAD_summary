f13a3135ac5be731146fcc50eb3b4f94d2f75f4a
Smith <Adam.Smith@ec.gc.ca>
2024-08-22 16:13:19 -0400

    alternate palette

--- plotting.R
+++ plotting.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(readxl)
 library(colorscience)
 library(RColorBrewer)
+library(ggstats)
 
 # ACAD globals ------------------------------------------------------------
 # x <- "aa%.( /#) -bb"
@@ -16,20 +17,51 @@ name_rep <- function(x){
      x <- str_replace_all(x,pattern = "_+","_")
      x <- str_to_lower(x)
 }
-acad <- read_xlsx("data/ACAD Global 2024.05.23.xlsx",
+acad1 <- read_xlsx("data/ACAD Global 2024.05.23.xlsx",
                   sheet = "Globals",
                   .name_repair = name_rep)
 
 
-acad <- acad %>% 
-  mutate(primary_breeding_habitat_major = str_extract(primary_breeding_habitat,
+acad <- acad1 %>% 
+  mutate(primary_breeding_habitat = str_replace_all(primary_breeding_habitat," Aerial",""),
+         primary_nonbreeding_habitat = str_replace_all(primary_nonbreeding_habitat," Aerial",""),
+         primary_breeding_habitat_major = str_extract(primary_breeding_habitat,
                                                       pattern = ".+(?=\\:)"),
          primary_breeding_habitat_sub = str_trim(str_extract(primary_breeding_habitat,
                                                       pattern = "(?<=\\:).+")),
+         primary_breeding_habitat_major = ifelse(primary_breeding_habitat_major == "Forest","Forests",primary_breeding_habitat_major),
+         
          primary_nonbreeding_habitat_major = str_extract(primary_nonbreeding_habitat,
                                                       pattern = ".+(?=\\:)"),
+         primary_nonbreeding_habitat_major = ifelse(primary_nonbreeding_habitat_major == "Forest","Forests",primary_nonbreeding_habitat_major),
+         
          primary_nonbreeding_habitat_sub = str_trim(str_extract(primary_nonbreeding_habitat,
-                                                             pattern = "(?<=\\:).+")))
+                                                             pattern = "(?<=\\:).+")),
+         status = continental_importance,
+         status = ifelse(grepl(continental_importance,pattern = "\\;"),
+                         str_extract(status, pattern = ".+(?=\\;)"),
+                         status),
+         status = ifelse(grepl(pattern = "Prev",status),
+                         NA,status),
+         #status = ifelse(!grepl(status,pattern = "Watch") | is.na(status),"",status),
+         status_alt = str_trim(str_extract(continental_importance,
+                                                             pattern = "(?<=\\;).+")),
+         tipping_point = ifelse(status_alt == "Tipping Point","/Tipping-Point",""),
+         status = paste0(status,tipping_point),
+         status = str_replace(status,"NA",replacement = ""),
+         status = ifelse(status == "NA" | is.na(status),"Non Watch List",status),
+         status = factor(status,
+                         levels = c("Non Watch List",
+                                    "Common Bird in Steep Decline",
+                                    "Yellow Watch List",
+                                    "Yellow Watch List/Tipping-Point",
+                                    "Orange Watch List/Tipping-Point",
+                                    "Red Watch List/Tipping-Point"),
+                         ordered = TRUE),
+         continental_importance = status
+         
+  )
+
 tst <- acad %>% 
   filter(is.na(ccs_max))
 
@@ -54,29 +86,35 @@ acad <- acad %>%
 colset2 <- rev(brewer.pal(11,"RdYlBu"))[c(1:5,6,7,7,8,8,9,10,10,11,11,11,11)]
 names(colset2) <- as.character(4:20)
 
-ft_mean <- function(x){
-  return(mean(as.integer(x)))
+
+col_names <- levels(acad$continental_importance)
+colset2 <- rev(brewer.pal(11,"RdYlBu"))[c(4,6,7,8,10,11)]
+names(colset2) <- col_names
+
+ft_mean <- function(x,thresh = 13){
+  return(length(which(as.integer(x)> thresh))/length(x))
 }
+
+
 # high level habitat summaries --------------------------------------------
 
 histo_function <- function(df = acad,
                grp = "primary_breeding_habitat_major",
-               sub_grp = NULL){
+               sub_grp = NULL,
+               scaling = "fixed"){
   
+
   if(is.null(sub_grp)){
     df[,"group_maj"] <- df[,grp]
   dfs <- df %>% 
-    group_by(group_maj,ccs_max) %>% 
-    summarise(n = n(),
-              .groups = "drop") %>% 
-    ungroup() %>% 
+    # group_by(group_maj,ccs_max) %>% 
+    # summarise(n = n(),
+    #           .groups = "drop") %>% 
+    # ungroup() %>% 
     mutate(base = 0,
            ccs_max = factor(ccs_max,
                       levels = as.character(4:20),
-                      ordered = TRUE)) %>% 
-    mutate(group_maj = fct_reorder(.f = group_maj,
-                                   .x = ccs_max,
-                                   .fun = ft_mean))
+                      ordered = TRUE)) 
   
   titl <- str_to_title(str_replace_all(grp,pattern = "_",replacement = " "))
   
@@ -84,9 +122,9 @@ histo_function <- function(df = acad,
     df[,"group_maj"] <- df[,grp]
     df[,"group_sub"] <- df[,sub_grp]
     dfs <- df %>% 
-      group_by(group_maj,group_sub,ccs_max) %>% 
-      summarise(n = n(),
-                .groups = "drop") %>% 
+      # group_by(group_maj,group_sub,ccs_max) %>% 
+      # summarise(n = n(),
+      #           .groups = "drop") %>% 
       ungroup() %>% 
       mutate(base = 0,
              ccs_max = factor(ccs_max,
@@ -94,28 +132,50 @@ histo_function <- function(df = acad,
                               ordered = TRUE))
   }
   
+  dfs <- dfs %>% 
+    filter(!is.na(group_maj))
+  
   if(is.null(sub_grp)){
+    
+    nsp <- dfs %>% 
+      group_by(group_maj) %>% 
+      summarise(nsps = n())
+    
+    dfs <- dfs %>% 
+      left_join(.,nsp,by = "group_maj") %>% 
+      mutate(group_maj = paste0(group_maj,"(",nsps,")"),
+             group_maj = fct_reorder(.f = group_maj,
+                                     .x = ccs_max,
+                                     .fun = ft_mean))
+    
   high1 <- ggplot(data = dfs,
-                  aes(y = n,
+                  aes(y = after_stat(prop),
                       x = ccs_max,
-                      colour = ccs_max))+
+                      by = 1,
+                      fill = continental_importance))+
     geom_vline(xintercept = c(8.5-3,13.5-3),
                colour = grey(0.8))+
-    geom_errorbar(aes(ymin = base,ymax = n),
-                  width = 0,
-                  linewidth = 10)+
-    scale_discrete_manual(aesthetics = "colour",
+    # geom_errorbar(aes(ymin = base,ymax = n),
+    #               width = 0,
+    #               linewidth = 10)+
+    scale_y_continuous(labels = scales::percent) +
+    geom_bar(stat = "prop",
+             width = 1)+
+    scale_discrete_manual(aesthetics = c("colour","fill"),
                           values = colset2)+
     facet_wrap(facets = vars(group_maj),
                ncol = 1,
-               scales = "free_y",
+               scales = scaling,
                strip.position = "top")+
     theme_minimal()+
     xlim(levels(dfs$ccs_max))+
     labs(title = titl)+
-    ylab("Number of Species")+
-    theme(legend.position = "none",
-          strip.text = element_text(size = 12))
+    ylab("Percent of Species")+
+    theme(legend.position = "right",
+          strip.text = element_text(size = 12),
+          panel.spacing.x=unit(0, "lines"))
+  
+  #high1
   
   }else{
     
@@ -127,8 +187,16 @@ histo_function <- function(df = acad,
     }
     for(jj in names(high1)){
       dfs1 <- dfs %>% 
-        filter(group_maj == jj) %>% 
-        mutate(group_sub = fct_reorder(.f = group_sub,
+        filter(group_maj == jj)
+      
+      nsp <- dfs1 %>% 
+        group_by(group_sub) %>% 
+        summarise(nsps = n())
+      
+      dfs1 <- dfs1 %>% 
+        left_join(.,nsp,by = "group_sub") %>% 
+        mutate(group_sub = paste0(group_sub,"(",nsps,")"),
+               group_sub = fct_reorder(.f = group_sub,
                                        .x = ccs_max,
                                        .fun = ft_mean))
       
@@ -136,26 +204,31 @@ histo_function <- function(df = acad,
       
       titl <- paste(jj)
       high1[[jj]] <- ggplot(data = dfs1,
-                      aes(y = n,
-                          x = ccs_max,
-                          colour = ccs_max))+
+                            aes(y = after_stat(prop),
+                                x = ccs_max,
+                                by = 1,
+                                fill = continental_importance))+
         geom_vline(xintercept = c(8.5-3,13.5-3),
                    colour = grey(0.8))+
-        geom_errorbar(aes(ymin = base,ymax = n),
-                      width = 0,
-                      linewidth = 10)+
-        scale_discrete_manual(aesthetics = "colour",
+        # geom_errorbar(aes(ymin = base,ymax = n),
+        #               width = 0,
+        #               linewidth = 10)+
+        scale_y_continuous(labels = scales::percent) +
+        geom_bar(stat = "prop",
+                 width = 1)+
+        scale_discrete_manual(aesthetics = c("colour","fill"),
                               values = colset2)+
         facet_wrap(facets = vars(group_sub),
                    ncol = 1,
-                   scales = "free_y",
+                   scales = scaling,
                    strip.position = "top")+
-        xlim(levels(dfs1$ccs_max))+
         theme_minimal()+
+        xlim(levels(dfs1$ccs_max))+
         labs(title = titl)+
         ylab("Number of Species")+
-        theme(legend.position = "none",
-              strip.text = element_text(size = 12))
+        theme(legend.position = "right",
+              strip.text = element_text(size = 12),
+              panel.spacing.x=unit(0, "lines"))
       
       
     }
